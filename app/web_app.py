6bcc8187b7c617604fb78d60128d98a5773ae05f
mauro <maurogioe91@libero.it>
2025-01-26 22:08:56 +0100

    implemented basic web app

--- app/web_app.py
+++ app/web_app.py
@@ -1,25 +1,48 @@
+import streamlit as st
+import subprocess
+import sys
 import ollama
-from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
+def scrape_and_save():
+    subprocess.run([f"{sys.executable}", "web_scraper.py"])
 
 
-def answer_the_question(question, context, context_window = 20000):
+def answer_the_question(question, num_doc_to_retrieve, context_window = 20000):
+    retriever = vectordb.as_retriever( search_kwargs={"k": num_doc_to_retrieve})
+    retrieved_doc = retriever.invoke(question)
+    print(retrieved_doc)
+    context = " ".join(doc.page_content for doc in retrieved_doc)
     response = ollama.chat(model='llama3.2:latest', messages=[
       {
         'role': 'user',
-        'content': f'''Answer the question according to the context given very briefly:
+        'content': f'''Answer the question according to the context given if question and context are related:
                        Question: {question}.
                        Context: {context}
                         ''',
       },
     ],  options={"temperature":0, "num_ctx":context_window})
-    return response["message"]["content"]
+    st.session_state['answer'] = response["message"]["content"]
+
+
+if 'question' not in st.session_state:
+    st.session_state['question'] = ''
+
+if 'answer' not in st.session_state:
+    st.session_state['answer'] = ''
+if 'num_doc_to_retrieve' not in st.session_state:
+    st.session_state['num_doc_to_retrieve'] = "1"
+
+st.button("Scrape video game news", on_click = scrape_and_save)
+
+st.text_input("What's the maximum number of news you want to hear about?", key="num_doc_to_retrieve")
+answer = st.text_input("Ask a video game news related question", key="question", on_change = answer_the_question,
+              args = (st.session_state.question, int(st.session_state.num_doc_to_retrieve)))
+
+st.write(st.session_state['answer'])
+
 
 local_embeddings = OllamaEmbeddings(model = "all-minilm")
-vectordb = Chroma(embedding_function=local_embeddings, collection_name = "game_news")
-question="tell me news about the switch 2"
-num_doc_to_retrieve = 2
-retriever = vectordb.as_retriever(search_type = "similarity", search_kwargs= {"k":num_doc_to_retrieve})
-retrieved_doc = retriever.invoke(question)
-context = " ".join(doc.page_content for doc in retrieved_doc)
-answer_the_question(question, context)
+vectordb = Chroma(persist_directory = "/dbfs/ChromaDB", embedding_function=local_embeddings, collection_name = "game_news")
+#st.write(vectordb.get())

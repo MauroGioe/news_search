8d426deacd482908feea205d3054c22f6771dcad
mauro <maurogioe91@libero.it>
2025-01-29 15:48:09 +0100

    Fixed double question bug

--- app/web_app.py
+++ app/web_app.py
@@ -11,7 +11,8 @@ def scrape_and_save():
     subprocess.run([f"{sys.executable}", "./web_scraper.py"])
 
 
-def answer_the_question(question, model, num_doc_to_retrieve, context_window = 20000):
+def answer_the_question(model, num_doc_to_retrieve, context_window = 20000):
+    question = st.session_state.question
     retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": num_doc_to_retrieve, "lambda_mult": 1})
     retrieved_doc = retriever.invoke(question)
     print(retrieved_doc)
@@ -43,7 +44,7 @@ vectordb = Chroma(persist_directory = "/dbfs/ChromaDB", embedding_function=local
 
 st.text_input("What's the maximum number of news you want to hear about?", key="num_doc_to_retrieve")
 st.text_input("Ask a video game news related question", key="question", on_change = answer_the_question,
-              args = (st.session_state.question, "qwen2.5:1.5b",  int(st.session_state.num_doc_to_retrieve)))
+              args = ("qwen2.5:1.5b",  int(st.session_state.num_doc_to_retrieve)))
 
 st.write(st.session_state['answer'])
 
